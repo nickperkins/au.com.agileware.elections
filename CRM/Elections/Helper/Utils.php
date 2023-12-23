e201920161c4584c5368a74c0e542fcdfb8b8804
Nick Perkins <nick@nicholasperkins.com>
2023-12-23 17:14:19 +1000

    Use contact image for election pages

--- CRM/Elections/Helper/Utils.php
+++ CRM/Elections/Helper/Utils.php
@@ -1,56 +1,37 @@
 <?php
 
-class CRM_Elections_Helper_Utils {
+class CRM_Elections_Helper_Utils
+{
 
-  public static function replaceSingleProfilePic(&$contact, $imageKey, $contactIdKey) {
+  public static function replaceSingleProfilePic(&$contact, $imageKey, $contactIdKey)
+  {
     $profilePics = self::getCMSProfilePictures(array($contact[$contactIdKey]));
     $contact[$imageKey] = $profilePics[$contact[$contactIdKey]];
   }
 
-  public static function getCMSProfilePictures($contactIds) {
+  public static function getCMSProfilePictures($contactIds)
+  {
     $profilePictures = array();
     if (count($contactIds) == 0) {
       return $profilePictures;
     }
-    $cmsMatches = civicrm_api3('UFMatch', 'get', [
-	  'sequential' => TRUE,
-      'return'     => ["uf_id", "contact_id"],
-      'contact_id' => ['IN' => $contactIds],
+
+    $cmsMatches = civicrm_api3('Contact', 'get', [
+      'sequential' => 1,
+      'return' => ["id", "image_URL"],
+      'id' => ['IN' => $contactIds],
       'options' => ['limit' => 0],
     ]);
 
-    $defaultEmails = civicrm_api3('Contact', 'get', [
-      'return' => ["email"],
-      'id' => ['IN' => $contactIds],
-	  'options' => ['limit' => 0],
-    ]);
+    $cmsMatches = $cmsMatches["values"];
 
-    $defaultEmails = $defaultEmails['values'];
 
-    $cmsMatches = $cmsMatches['values'];
-
-    foreach ($contactIds as $contactId) {
-      $contactEmail = (isset($defaultEmails[$contactId]['email'])) ? $defaultEmails[$contactId]['email'] : "";
-      $profilePictures[$contactId] = self::getGravatarUrlFromEmailId($contactEmail);
-    }
-
-    if (function_exists('add_filter')) {
-      foreach ($cmsMatches as $cmsMatch) {
-        $profilePictures[$cmsMatch['contact_id']] = get_avatar_url($cmsMatch['uf_id'], array(
-          'size' => 300,
-        ));
-      }
+    $defaultImage = Civi::resources()->getUrl('au.com.agileware.elections', 'images/default_profile.jpg');
+    
+    foreach ($cmsMatches as $cmsMatch) {
+      $profilePictures[$cmsMatch['contact_id']] = $cmsMatch['image_URL'] !== "" ? $cmsMatch['image_URL'] : $defaultImage;
     }
 
     return $profilePictures;
   }
-
-  private static function getEmailHashForGravatar($emailId) {
-    return md5(strtolower(trim($emailId)));
-  }
-
-  private static function getGravatarUrlFromEmailId($emailId) {
-    return "https://2.gravatar.com/avatar/" . self::getEmailHashForGravatar($emailId) . "?s=300&d=mm&r=g";
-  }
-
 }
